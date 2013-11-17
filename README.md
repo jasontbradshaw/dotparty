68274e8786f276ef65101b2f5923529ec04c41c4
Jason T. Bradshaw <jasontbradshaw@gmail.com>
2013-11-17 15:03:01 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+dotparty
+========
+
+Where all your dotfiles hang out on Friday night.
---
+++ dotparty.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+import sys
+import os
+
+def main(args):
+  pass
+
+if __name__ == '__main__':
+  main(sys.argv)
